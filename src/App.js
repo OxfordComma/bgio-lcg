2e5552728ff67c704d5b464f63bded4c2de89c72
Nik Matthiopoulos <nikolasmatt@gmai.com>
2022-05-28 23:51:11 -0400

    add player switching

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Client } from 'boardgame.io/react';
 import { Local, SocketIO } from 'boardgame.io/multiplayer';
 import { CardGame } from './Game';
 import { GameBoard } from './Board';
+import { PlayerSelectMenu } from './PlayerSelectMenu';
 
 import React, { useState, useEffect } from 'react'
 
@@ -15,27 +16,16 @@ const CardGameClient = Client({
 });
 
 const App = () => {
-  // let [playerID, setPlayerID] = useState(null);
-  // return (
-  //   playerID == null ? 
-  //   <div>
-  //     <form onSubmit={e => {e.preventDefault(); setPlayerID(e.target[0].value);}}>
-  //       <label for='playerID'>playerID: </label>
-  //       <select id='playerID' type='dropdown' >
-  //         <option value='0'>0</option>
-  //         <option value='1'>1</option>
-  //       </select>
-  //       <button type='submit'>submit</button>
-  //     </form>
-  //   </div> :
-  //   <div>
-  //     <CardGameClient playerID={playerID}/>
-  //   </div>
-  // )
+  let [playerID, setPlayerID] = useState(null);
+
+  function onPlayerSelect(selectedPlayerID) {
+    setPlayerID(selectedPlayerID);
+  }
+  
   return (
     <div>
-      <CardGameClient playerID="0"/>
-      <CardGameClient playerID="1"/>
+      <PlayerSelectMenu currentPlayer={playerID} onPlayerSelect={onPlayerSelect} />
+      {playerID && <CardGameClient playerID={playerID}/>}
     </div>
   )
 }
